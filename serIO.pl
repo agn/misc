9b04cfb0b439ba03d02aadc5d5f5b1da69a4cf86
Arun G Nair <arun.g.nair@gmail.com>
2009-06-10 18:20:18 +0530

    http server skeleton

--- serIO.pl
+++ serIO.pl
@@ -3,9 +3,10 @@
 use strict;
 use IO::Socket;
 
-my @files;
+my (@files, $req, $res);
 
 $SIG{'INT'} = \&cleanup;
+my $DOCROOT = '/home/arun/site/';
 
 my $socket = new IO::Socket::INET ( 
 	LocalAddr => '172.17.1.50',
@@ -20,18 +21,40 @@ print 'Listening on ', $socket->sockhost(),":", $socket->sockport, "\n";
 
 while (my $client = $socket->accept()) {
 	print 'Connected ', $client->peerhost(),":", $client->peerport(), "\n";
-	print $client scalar localtime,"\n";
-	$, = "\n";
-	print $client &getfiles(shift || '.'), "\n";
-
+# print HTTP request headers
+#	while (($req = <$client>)) {
+#		print $req;
+#		last if $req =~ /^\r\n$/;
+#	}
+	#get http request - first line
+	$req = <$client>;
+	print scalar localtime," - ", $client->peerhost()," - ", $req;
+	$res = &handle_req($req);
+	print $client $_ while (<$res>);
 	close $client;
 }
 
 sub cleanup { close $socket; die "Interrupted. Exiting...\n"; }
-sub getfiles {
-	my $dir = shift; 
-	opendir DIR, $dir or die "open:$!\n";
-	@files = grep { !/^\.(\.)?$/ } readdir DIR;
-	closedir DIR;
-	return @files;
+#sub getfiles {
+#	my $dir = shift; 
+#	opendir DIR, $dir or die "open:$!\n";
+#	@files = grep { !/^\.(\.)?$/ } readdir DIR;
+#	closedir DIR;
+#	return @files;
+#}
+sub handle_req {
+	my ($method, $URI) = split / +/, shift;
+	my ($file, $invalid_req);
+	if ($method !~ /GET/) {
+		$invalid_req = 1;
+		$file = '404.html';
+	}
+	unless ($invalid_req) {
+		$URI =~ s/\/(.*)/$1/;				# strip the first slash
+		$file = $URI || 'index.html';
+		-f $DOCROOT.$file or $file = '404.html';
+	}
+	open $res, $DOCROOT.$file or die "open: $!";
+	print "Sending $file...\n";
+	return $res;
 }
