638de89bd02b014d496f95ce2f2f6ffcb76ab833
Arun G Nair <arun.g.nair@gmail.com>
2009-06-11 18:12:10 +0530

    added directory index functionality
    needs cleanup

--- serIO.pl
+++ serIO.pl
@@ -1,11 +1,16 @@
-#!/usr/local/bin/perl -w
+#!/usr/local/bin/perl
+
+#Caution: Big mess ahead
 
 use strict;
+use warnings;
+
 use IO::Socket;
 
-my (@files, $req, $res);
+my (@files, $method, $req, $res, $URI, $pipe);
 
-$SIG{'INT'} = \&cleanup;
+
+#$SIG{'INT'} = \&cleanup;
 my $DOCROOT = '/home/arun/site/';
 
 my $socket = new IO::Socket::INET ( 
@@ -20,21 +25,33 @@ $socket->listen();
 print 'Listening on ', $socket->sockhost(),":", $socket->sockport, "\n";
 
 while (my $client = $socket->accept()) {
-	print 'Connected ', $client->peerhost(),":", $client->peerport(), "\n";
+	&log("Connected ".$client->peerhost().":".$client->peerport()."\n");
 # print HTTP request headers
 #	while (($req = <$client>)) {
 #		print $req;
 #		last if $req =~ /^\r\n$/;
 #	}
-	#get http request - first line
+# get http request - first line
 	$req = <$client>;
-	print scalar localtime," - ", $client->peerhost()," - ", $req;
+	&log($client->peerhost()." ".$req);
 	$res = &handle_req($req);
-	print $client $_ while (<$res>);
+	if (-f $res) {
+	   	open RES, $res, or die "open: $res: $!"; 
+		print $client $_ while (<RES>);
+	} else {
+		$, = "\n";
+		foreach (@$res) {
+			print $client $_."\n";
+		}
+	}
 	close $client;
 }
 
-sub cleanup { close $socket; die "Interrupted. Exiting...\n"; }
+#sub cleanup { close $socket; die "Interrupted. Exiting...\n"; }
+sub log { 
+	my $msg = shift;
+	print scalar localtime," ", $msg;
+}
 #sub getfiles {
 #	my $dir = shift; 
 #	opendir DIR, $dir or die "open:$!\n";
@@ -43,18 +60,37 @@ sub cleanup { close $socket; die "Interrupted. Exiting...\n"; }
 #	return @files;
 #}
 sub handle_req {
-	my ($method, $URI) = split / +/, shift;
+	($method, $URI) = split / +/, shift;
 	my ($file, $invalid_req);
 	if ($method !~ /GET/) {
 		$invalid_req = 1;
-		$file = '404.html';
+		$URI = '404.html';
 	}
 	unless ($invalid_req) {
-		$URI =~ s/\/(.*)/$1/;				# strip the first slash
-		$file = $URI || 'index.html';
-		-f $DOCROOT.$file or $file = '404.html';
+		$URI =~ s/\/(.*)/$1/;								# strip the first slash
+		# TODO: CLEAN UP THIS MESS
+		if (!$URI || -d $DOCROOT.$URI) {
+			if (-f "$DOCROOT$URI".'index.html') {
+				$URI .= 'index.html';
+			} else {
+			   	&log('dir_index'."\n"); #&build_dirindex($DOCROOT.$URI);
+			}
+		} else {
+			-f $DOCROOT.$URI || &log('404'."\n") && defined($URI = '404.html');
+		}
 	}
-	open $res, $DOCROOT.$file or die "open: $!";
-	print "Sending $file...\n";
+	if (-f $DOCROOT.$URI) {
+#	   	open $res, $DOCROOT.$URI or die "open: $DOCROOT$URI : $!"; 
+		$res = $DOCROOT.$URI;
+	}
+	-d $DOCROOT.$URI and $res = &build_dirindex($DOCROOT.$URI);
+	&log("Sending $DOCROOT$URI...\n");
 	return $res;
+}
+sub build_dirindex {
+	my $dir = shift;
+	opendir DIR, $dir or die "open:$!\n";
+	@files = grep { !/^\.(\.)?$/ } readdir DIR;
+	closedir DIR;
+	\@files;
 }
